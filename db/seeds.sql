545f490b9efe09786e353da07e2ea52802e847a8
Sara Anspach <sara.anspach@gmail.com>
2018-10-02 21:35:54 -0400

    first

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburguer', 'Albert Bahia');
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Bacon', "Theodore Roosevelt");
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Triple Meat', "Owens O'Brien");
